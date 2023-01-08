e1bedb3da4af7f35c5e9e5c9dd6c7c3341621b2f
Mark Zemlianskyi <mark080501@gmail.com>
2023-01-08 16:07:01 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// const refs = {
+// targetContainer: document.querySelector("#ingredients"),
+// };
+const targetContainer = document.querySelector("#ingredients");
+const makeIngredient = (ingredients) => {
+  return ingredients.map((i) => {
+    const item = document.createElement("li");
+    item.classList.add("item");
+    item.textContent = i;
+    targetContainer.append(item);
+  });
+};
+console.log(makeIngredient(ingredients));
