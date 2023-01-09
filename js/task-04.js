4fee0cd36853471545e95c28a221dd720ae1b23c
Mark Zemlianskyi <mark080501@gmail.com>
2023-01-09 08:10:06 +0200

    task 4

---
+++ js/task-04.js
@@ -0,0 +1,32 @@
+const refs = {
+  decrement: document.querySelector('[data-action="increment"]'),
+  increment: document.querySelector('[data-action="decrement"]'),
+  value: document.querySelector("#value"),
+};
+refs.decrement.addEventListener("click", onDecrementClick);
+refs.increment.addEventListener("click", onIncrementClick);
+
+// const counterValue = {
+//   value: 0,
+//   increase() {
+//     this.value += 1;
+//   },
+//   decrease() {
+//     this.value -= 1;
+//   },
+// };
+
+let counterValue = 0;
+function onDecrementClick() {
+  // counter.increase();
+  counterValue += 1;
+  // refs.value.textContent = counter.value;
+  refs.value.textContent = counterValue;
+}
+function onIncrementClick() {
+  // counter.decrease();
+  counterValue -= 1;
+  refs.value.textContent = counterValue;
+
+  // refs.value.textContent = counter.value;
+}
