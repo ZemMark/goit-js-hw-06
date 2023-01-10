0db978d2a46d54cf210258824c991d9a44063171
Mark Zemlianskyi <mark080501@gmail.com>
2023-01-10 18:11:47 +0200

    10th task. half of the path...

--- js/task-10.js
+++ js/task-10.js
@@ -1,52 +1,96 @@
-const input = document.querySelector("input");
+// const input = document.querySelector("input");
 
-const bgc = getRandomHexColor();
-let startSize = 30;
-const divEl = {
-  color: bgc,
-  width: 30,
-  height: 30,
-};
-const onInputChange = (event) => {
-  const value = event.currentTarget.value;
-  return value;
-};
-input.addEventListener("input", onInputChange);
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-function makeNewSize(value) {
-  startSize += 10;
-}
+// const bgc = getRandomHexColor();
+// let startSize = 30;
+// const divEl = {
+//   color: bgc,
+//   width: 30,
+//   height: 30,
+// };
+// const onInputChange = (event) => {
+//   const value = event.currentTarget.value;
+//   return value;
+// };
+// input.addEventListener("input", onInputChange);
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// function makeNewSize(value) {
+//   startSize += 10;
+// }
 
-// const createBox = function (value) {
+// // const createBox = function (value) {
+// //   for (let i = 0; i <= value; i++) {
+// //     const divEl = `<div width="${startSize}", height="${startSize}" backgroundColor="${bgc}"></div>`;
+// //     document.body.insertAdjacentHTML("beforeend", divEl);
+// //   }
+// // };
+
+// //try loDash range
+
+// const valueArray = [];
+// const fnArray = (value) => {
 //   for (let i = 0; i <= value; i++) {
-//     const divEl = `<div width="${startSize}", height="${startSize}" backgroundColor="${bgc}"></div>`;
-//     document.body.insertAdjacentHTML("beforeend", divEl);
+//     valueArray.push(i);
 //   }
+//   return valueArray;
+// };
+// console.log(fnArray(4));
+
+// const makeDivEl = function (array) {
+//   return array
+//     .map((el) => {
+//       el = `<div width="${makeNewSize()}px", height="${makeNewSize()}px" background-color="${getRandomHexColor()}" class="divsLastTask"></div>`;
+//       return el;
+//     })
+//     .join("");
 // };
 
-//try loDash range
+// // const makeDivRefs = valueArray.map();
+// console.log(valueArray);
+// console.log(makeDivEl(valueArray));
+// document.body.insertAdjacentHTML("beforeend", makeDivEl(valueArray));
+const input = document.querySelector("input");
+const boxesContainer = document.querySelector("#boxes");
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const iterationArray = [];
+let initialSizeValue = 30;
 
-const valueArray = [];
-const fnArray = (value) => {
-  for (let i = 0; i <= value; i++) {
-    valueArray.push(i);
-  }
-  return valueArray;
+//
+function makeSizeValue() {
+  const iterableSizeValue = (initialSizeValue += 10);
+  return iterableSizeValue;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const onCreateClick = function () {
+  boxesContainer.innerHTML = makeDiv();
 };
-console.log(fnArray(4));
-
-const makeDivEl = function (array) {
-  return array
+const onDestroyClick = () => {
+  boxesContainer.innerHTML = "";
+  iterationArray = [];
+};
+const makeIterationCountArray = function () {
+  const iterations = input.value;
+  for (let i = 0; i < iterations; i += 1) {
+    iterationArray.push(i);
+    // const divRef = `<div class="divsLastTask", width="", height=""></div>`;
+  }
+  return iterationArray;
+};
+const makeDiv = function (e) {
+  makeIterationCountArray();
+  return iterationArray
     .map((el) => {
-      el = `<div width="${makeNewSize()}px", height="${makeNewSize()}px" background-color="${getRandomHexColor()}" class="divsLastTask"></div>`;
-      return el;
+      const createdDiv = `<div class="divsLastTask" width="${makeSizeValue()}" height="" background-color="${getRandomHexColor()}"></div>`;
+      return createdDiv;
     })
     .join("");
 };
-
-// const makeDivRefs = valueArray.map();
-console.log(valueArray);
-console.log(makeDivEl(valueArray));
-document.body.insertAdjacentHTML("beforeend", makeDivEl(valueArray));
+btnCreate.addEventListener("click", onCreateClick);
+btnDestroy.addEventListener("click", onDestroyClick);
