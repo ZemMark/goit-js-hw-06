6771380c322622d0dca9e9dc4c88424f9fd3f762
Mark Zemlianskyi <mark080501@gmail.com>
2023-01-11 15:32:44 +0200

    9th task fix

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+// const button = document.querySelector("button");
+// const span = document.querySelector("span");
+
+// button.addEventListener("click", onButtonClick);
+// function onButtonClick() {
+//   document.body.style.backgroundColor = getRandomHexColor();
+//   span.textContent = getRandomHexColor();
+// }
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
@@ -5,9 +19,9 @@ function getRandomHexColor() {
 }
 const button = document.querySelector("button");
 const span = document.querySelector("span");
-
 button.addEventListener("click", onButtonClick);
 function onButtonClick() {
-  document.body.style.backgroundColor = getRandomHexColor();
-  span.textContent = getRandomHexColor();
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
 }
