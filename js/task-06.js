f7e28a5b01393e221519270dc3d2ad0ce165ddf3
Mark Zemlianskyi <mark080501@gmail.com>
2023-01-11 15:01:04 +0200

    debug of the 6th task

--- js/task-06.js
+++ js/task-06.js
@@ -5,9 +5,12 @@ input.addEventListener("blur", onInputBlur);
 function onInputBlur(event) {
   const dataLength = event.currentTarget.getAttribute("data-length");
   // console.log(event.currentTarget.getAttribute("data-length"));
-  if (event.currentTarget.value.length < dataLength) {
-    input.classList.add("invalid");
-  } else if (event.currentTarget.value.length >= dataLength)
+  // if (event.currentTarget.value.length < dataLength) {
+  // input.classList.add("invalid");
+  if (event.currentTarget.value.length === Number(dataLength)) {
+    input.classList.add("valid");
     input.classList.remove("invalid");
-  input.classList.add("valid");
+  } else {
+    input.classList.add("invalid");
+  }
 }
